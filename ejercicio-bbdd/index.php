3941fdce422d0aea331ccf955e24fb40f76eecf2
iZicox <53028086+iZicox@users.noreply.github.com>
2026-03-26 13:49:04 +0100

    pagina de inicio con bloqueo

--- ejercicio-bbdd/index.php
+++ ejercicio-bbdd/index.php
@@ -2,3 +2,102 @@
 
 // pagina privada que si el usuario no esta logeado oculta el contenido privado y solo muestra el link al login
 // si el usuario esta logeado muestra el contenido privado
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            gap: 5rem;
+            height: 100vh;
+        }
+        .img {
+            width: 20rem;
+            height: 15rem;
+            background-color: red;
+        }
+        body section:nth-of-type(2){
+            display: flex;
+            flex-wrap: wrap;
+            align-items: center;
+            justify-content: center;
+            gap: 1rem;
+        }
+        h1{
+            font-size: 4rem;
+        }
+        .block {
+            width: 100vw;
+            height: 100vh;
+            background: rgb(0,0,0,0.6);
+            backdrop-filter: blur(20px);
+            position: fixed;
+            top: 0;
+            left: 0;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        .block > * {
+            font-size: 3rem;
+            color: white;
+            font-weight: 500;
+        }
+
+        .disable {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <section>
+        <h1>Contenido privado</h1>
+    </section>
+    <section>
+        <div class="img">
+
+        </div>
+        <div class="img">
+            
+        </div>
+        <div class="img">
+            
+        </div>
+        <div class="img">
+            
+        </div>
+        <div class="img">
+
+        </div>
+        <div class="img">
+            
+        </div>
+        <div class="img">
+            
+        </div>
+        <div class="img">
+            
+        </div>
+
+    </section>
+    <div class="block">
+        <p>Usuario no autenticado</p>
+        <a href="login.php">Click para iniciar sesion</a>
+    </div>
+</body>
+</html>
