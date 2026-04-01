ff5532beaec28e9d38d0b8471f1341f1f9bd15dd
iZicox <53028086+iZicox@users.noreply.github.com>
2026-04-01 20:35:04 +0200

    Update varios.php

--- ejercicio-bbdd/varios.php
+++ ejercicio-bbdd/varios.php
@@ -2,6 +2,36 @@
 
 // funciones de utilidad
 function conectarPDO($ip,$puerto,$dbname,$user,$pass):PDO {
-    return new PDO("mysql:host=$ip;port=$puerto;dbname=$dbname",$user,$pass);
+    $pdo = new PDO("mysql:host=$ip;port=$puerto;dbname=$dbname",$user,$pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
 }
 
+function fetchUsuarioCookie($cookieId){
+    $conexion = conectarPDO("localhost","3307","ejemplosphp","root","");
+    $consulta = "select * from usuarios where codigocookie = :codigocookie";
+    $pre = $conexion->prepare($consulta);
+    $pre->execute(
+        [
+            ":codigocookie" => $cookieId
+        ]
+    );
+    $datos = $pre->fetch(PDO::FETCH_ASSOC);
+    return $datos;
+}
+
+function insertUsuario($conexion,$identificador,$nombre,$apellidos,$contrasenna):void {
+    // consulta del insert
+            $plantillaRegistro = "insert into usuarios(identificador,nombre,apellidos,contrasenna) values(:identificador,:nombre,:apellidos,:contrasenna)";
+
+            // creando la consulta y agregandole los datos del post
+            $insert = $conexion->prepare($plantillaRegistro);
+            $insert->execute(
+                array(
+                    ":identificador" => $identificador,
+                    ":nombre" => $nombre,
+                    ":apellidos" => $apellidos,
+                    ":contrasenna" => $contrasenna
+                )
+            );
+}
